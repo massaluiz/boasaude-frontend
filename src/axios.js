f5db7da2c1a81170f4e7e82c352fb1d464f7ce35
Luiz Massa de Pontes <luimassapontes@gmail.com>
2022-09-17 16:22:20 -0300

    Fixing andress to deploy

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//axios.defaults.baseURL = 'https://boasaude-backend.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://boasaude-backend.herokuapp.com';
+//axios.defaults.baseURL = 'http://localhost:8080';
 
 export default axios;
